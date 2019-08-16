2a32f8420b998b19647f8b3a405abb890cbc6e47
Artur Brasil <arturmbrasil@hotmail.com>
2019-08-15 23:51:40 -0300

    Updates

---
+++ precache-manifest.96fa5366612660626db42f227e4eab24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984295d03802161ae67eb13d502bc86d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c17dbcf1b2a11fcf511",
+    "url": "/static/css/main.5c5c7f73.chunk.css"
+  },
+  {
+    "revision": "9e48dfed12813631cf8b",
+    "url": "/static/js/2.87eff47b.chunk.js"
+  },
+  {
+    "revision": "3c17dbcf1b2a11fcf511",
+    "url": "/static/js/main.2604b4df.chunk.js"
+  },
+  {
+    "revision": "6db26e6357ebef89ec53",
+    "url": "/static/js/runtime~main.9a890e17.js"
+  }
+]);
